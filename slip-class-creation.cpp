0075e81b34318160071b27f4eb08692e6652d7c3
Grant Fisher <easyg53@gmail.com>
2014-04-24 18:51:31 -0500

    Separates a few member functions from 'slip-class' for the new 'yard-class'.

--- slip-class-creation.cpp
+++ slip-class-creation.cpp
@@ -3,7 +3,8 @@
 *      program name:       Slip-Class
 *      Author:             Grant & Cruze
 *      date due:           Hopefully by the end of the semester...
-*      remarks:            Still needs a lot of work.
+*      remarks:            This class is only for calculating the price and type
+*                          of a given length. (If the price is ridiculous, I apologize)
 ***************************************************/
 
 /******************************************
@@ -15,7 +16,6 @@
 /******************************************
 *     pre-processor
 ******************************************/
-#define PI 3.14159
 using namespace std;
 
 /****************************************
@@ -29,59 +29,36 @@ class Slip
               int type;
       public:
              //Constructor
-             Slip();             //Constructor that assigns size of boat and ID no.
+             Slip();             //Default Constructor that defaults size of boat to 0. Cruze made it.
              Slip(int length);
-
-             //Destructor
-             ~Slip()
-             {
-                 cout << "slip destructor";
-             }
 
              //setters
              int setLength(int x)              {length = x;}
              int setPrice(int x)               {price = x;}
              int setType(int x)                {type = x;}
-             //int setSlip(int x)                {slip = x;}
 
              //getters
              int getLength()                   {return length;}
              int getPrice()                    {return price;}
              int getType()                     {return type;}
-             //int getSlip()                     {return slip;}
 
              //Member function prototypes
-             bool vacancy(int id);       //Identifies the vacancy of a slip by passing its id no. in the parameters.
-             void display();             //Displays the slip ID number, length, type and its vacancy.
+             void display();             //Displays the slip length, type, and price 
 
 };
 /****************************************
-*       enum
+*         enum
 ****************************************/
 enum type {small, medium, large, unknown};
 
 /****************************************
 *         member functions
 ****************************************/
-///Constructors:
-///This constructor creates an array to assign each slip an ID no.(1-100)
+///Constructor:
 Slip :: Slip()
 {
     length = 0;
     price = 0;
-    int x = 1; //counter for future use
-    int slipMap[1][100]; //initializes 1 by 100 array for slips
-
-    ///Gives the ID no. to slip array (no. 1-100)
-    for (x=1; x<101; x++)
-        {
-              slipMap[0][x] = x;
-              //cout << slipMap[0][x] << " ";   //Uncomment this line to have the array printed out
-        }
-
-
-
-
 }
 
 ///This constructor sets the type and price of boat slip based on the length defined in the parameters
@@ -115,17 +92,10 @@ Slip :: Slip(int length)
         }
 }
 
-///This function will return the vacancy of a slip
-///'0' = empty;     '1' = occupied;
-bool Slip :: vacancy(int id)
-{
-}
-
+/// Displays the length, type and price
 void Slip :: display()
 {
-    cout << "length:            "     << getLength() << "\n";
-    cout << "price:             "     << getPrice() << "\n";
-    cout << "slip number:      \n"; //Haven't completed the function yet.
+    cout << "length:            "     << getLength() << " feet\n";
     cout << "type:              ";
     switch(getType())
     {
@@ -137,32 +107,21 @@ void Slip :: display()
             break;
         case 4: cout << "unknown\n";
     }
-    cout << "vacancy:          \n";
-/*    switch(getUse())
-    {
-        case 0: cout << "Open\n";
-            break;
-        case 1: cout << "Occupied\n";
-            break;
-        case 2: cout << "unknown\n";
-    }*/
+    cout << "price:             " << getPrice() << "$/month\n";
 }
 
-/****************************************
-*          function prototypes
-****************************************/
 
 /*****************************************
 *   main() - the function that executes
 *****************************************/
 int main()
 {
-    /*******************************
-    *     local variables
-    ******************************/
-    Slip test(10);          //tests constructo
-    test.display();         //tests display function
-    cout << "\n\n";
+    Slip test(10);          //Member function tests
+    test.display();        
+    cout << "\n";
+    Slip test2(75);
+    test2.display();
+    cout << "\n";
 
 	return 0;
 }  // end main
